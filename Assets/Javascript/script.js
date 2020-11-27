729cdcebff0d15f48e78d7b7aee0dad86e6d83f7
unknown <pfotis@gmail.com>
2020-11-27 18:19:04 +1100

    in case the user give us none answer for the length and the style

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -14,6 +14,20 @@ var isLowerCase = confirm("Do you want Lower Cases characters in your password ?
 var isNumber = confirm("Do you want numeric characters in your password ?");
 var isSpecial = confirm("Do you want special characters in your password ?");
 
+// in case the user give us out the range 8 and 128
+while(counter<8 || counter>128){
+  counter = prompt("Choose the length of your password between 8 and 128 characters ?");
+}
+
+// in case the user didnt choose any of the division of characters
+while(!isLowerCase && !isUpperCase && !isNumber && !isSpecial ){
+  alert("You have to answer positive at least one of the next question to be able to create the password :");
+  isUpperCase = confirm("Do you want Upper Cases characters in your password ?");
+  isLowerCase = confirm("Do you want Lower Cases characters in your password ?");
+  isNumber = confirm("Do you want numeric characters in your password ?");
+  isSpecial = confirm("Do you want special characters in your password ?");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
