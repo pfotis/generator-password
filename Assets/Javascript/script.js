0a867a1491c6616f1d9c5ba4032dac7e52d39326
unknown <pfotis@gmail.com>
2020-11-27 18:17:01 +1100

    create the question for the formula of the password

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -7,6 +7,12 @@ var upperCaseArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S"
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "<", ">", "?", ";", ":", "/", "?", "|", "-", "+", "~", "`", "[", "]","{","}"];
 
+// Questions for the formula of the password
+var counter = prompt("Choose the length of your password between 8 and 128 characters ?");
+var isUpperCase = confirm("Do you want Upper Cases characters in your password ?");
+var isLowerCase = confirm("Do you want Lower Cases characters in your password ?");
+var isNumber = confirm("Do you want numeric characters in your password ?");
+var isSpecial = confirm("Do you want special characters in your password ?");
 
 // Write password to the #password input
 function writePassword() {
