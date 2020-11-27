9d10b8a00cb6c72bf180e5317d4ab1d4e40ee84b
unknown <pfotis@gmail.com>
2020-11-27 18:15:29 +1100

    create arrays into javascript file

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays
+var passwordArray = new Array();
+var upperCaseArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "<", ">", "?", ";", ":", "/", "?", "|", "-", "+", "~", "`", "[", "]","{","}"];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
