ef321f64757871170948016ae6a3e916ff5590cc
unknown <pfotis@gmail.com>
2020-11-28 20:24:18 +1100

    create loop while in case the user didn't give any answer to crerate the password and create fuction for the random numbers

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,16 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Arrays
-var passwordArray = new Array();
-var categoriesOfKeys = new Array()
-var upperCaseArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
-var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "<", ">", "?", ";", ":", "/", "?", "|", "-", "+", "~", "`", "[", "]","{","}"];
-
-var counterType = 0;
-var checkTypes =0;
-var totalTypes = 0;
 
 // Questions for the formula of the password
 var lenghtOfArray = prompt("Choose the length of your password between 8 and 128 characters ?");
@@ -32,61 +22,75 @@ while(!isLowerCase && !isUpperCase && !isNumber && !isSpecial ){
   isNumber = confirm("Do you want numeric characters in your password ?");
   isSpecial = confirm("Do you want special characters in your password ?");
 }
-// check how many differents types will include in the password
-if(isUpperCase){
-  categoriesOfKeys[counterType] = 0;
-  counterType++;
-}
-if(isLowerCase){
-  categoriesOfKeys[counterType] = 1;
-  counterType++;
-}
-if(isNumber){
-  categoriesOfKeys[counterType] = 2;
-  counterType++;
-}
-if(isSpecial){
-  categoriesOfKeys[counterType] = 3;
-  counterType++;
-}
-for( var k=0; k<counterType; k++){
-  console.log(categoriesOfKeys[k]);
-}
-console.log("---------------------");
-console.log("---------------------");
-console.log("total : " + counterType);
-console.log("---------------------");
-/* the while help to fill the password array and to check if at least 
-  we have one time each categories of the user's choices */
-while (totalTypes != counterType){
-  for (var i=0; i<lenghtOfArray; i++){
-    passwordArray[i]=categoriesOfKeys[Math.floor(Math.random()*counterType)]; 
-    console.log("arrayspot " + i + "-->" +  passwordArray[i]);
-  }
-  for ( var j=0; j<counterType; j++){
-    for( i=0; i<lenghtOfArray; i++){
-      if ( categoriesOfKeys[j] === passwordArray[i]){
-        checkTypes=1;
-      }
-    }
-    totalTypes=totalTypes+checkTypes;
-  }
-  console.log("total : " + totalTypes);
-}
-for (i=0; i<lenghtOfArray; i++){
 
+
+function generatePassword() {
+  // Arrays
+  var passwordArray = new Array();
+  var categoriesOfKeys = new Array()
+  var upperCaseArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+  var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "<", ">", "?", ";", ":", "/", "?", "|", "-", "+", "~", "`", "[", "]","{","}"];
+
+  var counterType = 0;
+  var checkTypes =0;
+  var totalTypes = 0;
+
+  // fuction creates  random numbers
+  function random(x) {
+    var result = Math.floor(Math.random()*x);
+    return result;
+  };
+
+
+  // check how many differents types will include in the password
+  if(isUpperCase){
+    categoriesOfKeys[counterType] = 0;
+    counterType++;
+  }
+  if(isLowerCase){
+    categoriesOfKeys[counterType] = 1;
+    counterType++;
+  }
+  if(isNumber){
+    categoriesOfKeys[counterType] = 2;
+    counterType++;
+  }
+  if(isSpecial){
+    categoriesOfKeys[counterType] = 3;
+    counterType++;
+  }
+
+  /* the while help to fill the password array and to check if at least 
+    we have one time each categories of the user's choices */
+  while (totalTypes != counterType){
+    for (var i=0; i<lenghtOfArray; i++){
+      passwordArray[i]=categoriesOfKeys[random(counterType)]; 
+    }
+    for ( var j=0; j<counterType; j++){
+      for( i=0; i<lenghtOfArray; i++){
+        if ( categoriesOfKeys[j] === passwordArray[i]){
+          checkTypes=1;
+        }
+      }
+      totalTypes=totalTypes+checkTypes;
+    }
+  }
+  
 }
-console.log("---------------------");
+
+
+
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+ };
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", function() {
+   generatePassword();
+});
