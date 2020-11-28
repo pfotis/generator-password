f67e690cd1789efdab26b4fe8bbbd79576e8eeac
unknown <pfotis@gmail.com>
2020-11-28 20:38:52 +1100

    change the question in prompt and alert and comments

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Questions for the formula of the password
 var lenghtOfArray = prompt("Choose the length of your password between 8 and 128 characters ?");
 var isUpperCase = confirm("Do you want Upper Cases characters in your password ?");
@@ -11,20 +10,21 @@ var isSpecial = confirm("Do you want special characters in your password ?");
 
 // in case the user give us out the range 8 and 128
 while(lenghtOfArray<8 || lenghtOfArray>128){
-  lenghtOfArray = prompt("Choose the length of your password between 8 and 128 characters ?");
+  lenghtOfArray = prompt("You need to choose the length of your password between 8 and 128 characters ?");
 }
 
 // in case the user didnt choose any of the division of characters
 while(!isLowerCase && !isUpperCase && !isNumber && !isSpecial ){
-  alert("You have to answer positive at least one of the next question to be able to create the password :");
+  alert("You need to answer positive at least one of the next questions and the webpage will be able to create the password :");
   isUpperCase = confirm("Do you want Upper Cases characters in your password ?");
   isLowerCase = confirm("Do you want Lower Cases characters in your password ?");
   isNumber = confirm("Do you want numeric characters in your password ?");
   isSpecial = confirm("Do you want special characters in your password ?");
 }
 
-
+// with the data the user insert and the following fuction we create the password 
 function generatePassword() {
+
   // Arrays
   var passwordArray = new Array();
   var categoriesOfKeys = new Array()
@@ -41,7 +41,6 @@ function generatePassword() {
     var result = Math.floor(Math.random()*x);
     return result;
   };
-
 
   // check how many differents types will include in the password
   if(isUpperCase){
@@ -96,10 +95,6 @@ function generatePassword() {
   }  
   document.getElementById("password").innerHTML = keyword;
 }
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
